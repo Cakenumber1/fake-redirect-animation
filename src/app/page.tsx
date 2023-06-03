a84afea381bdfd035cd53175c6a7b4560bbb364d
Mikhail Nikolaev <cakenumber1@mail.ru>
2023-06-04 02:27:58 +0300

    fix text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -139,9 +139,6 @@ export default function Home() {
                 </button>
 
                 <div className={styles.content}>
-                    <p><b>This example demonstrates how to hide a navbar when the user starts to scroll the page.</b></p>
-                    <p>Scroll down this frame to see the effect!</p>
-                    <p>Scroll up to show the navbar.</p>
                     <p>Lorem ipsum dolor dummy text sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
                         ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
                         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
