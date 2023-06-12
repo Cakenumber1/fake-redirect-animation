6e8839c8abbef56ac4e052885523572ba443154f
Mikhail Nikolaev <cakenumber1@mail.ru>
2023-06-12 17:20:14 +0300

    upd Nav forwardRef

--- src/app/modules/navigation/Nav.tsx
+++ src/app/modules/navigation/Nav.tsx
@@ -1,19 +1,21 @@
 import styles from './nav.module.scss'
-import React from 'react';
+import React, { ReactElement, Ref } from 'react';
 import cx from 'classnames'
 
 type NavProps = {
   showNav: boolean
 }
 
-const Nav = React.forwardRef<HTMLElement>(({showNav}: NavProps, ref) => (
-  <nav ref={ref} className={cx(styles.navbar, {[styles.show]: showNav})}>
+const NavComponent = (props: NavProps, ref: Ref<HTMLElement>) => (
+  <nav ref={ref} className={cx(styles.navbar, {[styles.show]: props.showNav})}>
     <a href="#home">Home</a>
     <a href="#news">News</a>
     <a href="#contact">Contact</a>
   </nav>
-));
+)
 
-Nav.displayName = 'Nav';
+// Cast the output
+const Nav = React.forwardRef(NavComponent) as
+  <T extends NavProps>(props: NavProps & { ref?: Ref<HTMLElement> }) => ReactElement
 
 export default Nav
